27886f8062c9ec6e54c007a67aabb3ac256150f5
Yusuf Hasan Miyan <yusuf.hasanmiyan@gmail.com>
2017-09-01 14:51:17 +1000

    YHMS-000: Configured the VM.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,3 +359,18 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
+
+// Local environment.
+$aliases['yhmsandbox.local'] = array(
+  'root' => '/var/www/yhmsandbox/docroot',
+  'uri' => 'http://yhmsandbox.local',
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['yhmsandbox.local'] += array(
+    'remote-host' => 'yhmsandbox.local',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
